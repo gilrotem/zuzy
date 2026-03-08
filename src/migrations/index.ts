de208ac4d0c8a372d240c381066bf11514779e75
gilrotem <gpr@gilrotem.com>
2026-03-08 13:49:02 +0200

    style(migrations): fix formatting in migrations index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,15 @@
-import * as migration_20260303_161746_brand_docs_init from './20260303_161746_brand_docs_init';
-import * as migration_20260308_120000_media_prefix_fix from './20260308_120000_media_prefix_fix';
+import * as migration_20260303_161746_brand_docs_init from './20260303_161746_brand_docs_init'
+import * as migration_20260308_120000_media_prefix_fix from './20260308_120000_media_prefix_fix'
 
 export const migrations = [
   {
     up: migration_20260303_161746_brand_docs_init.up,
     down: migration_20260303_161746_brand_docs_init.down,
-    name: '20260303_161746_brand_docs_init'
+    name: '20260303_161746_brand_docs_init',
   },
   {
     up: migration_20260308_120000_media_prefix_fix.up,
     down: migration_20260308_120000_media_prefix_fix.down,
-    name: '20260308_120000_media_prefix_fix'
+    name: '20260308_120000_media_prefix_fix',
   },
-];
+]

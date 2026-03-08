2a31931525e5358c4f427e6c812e2b0bce6d73af
gilrotem <gpr@gilrotem.com>
2026-03-08 10:59:56 +0200

    fix: allow Supabase S3 host in Next image remote patterns

--- next.config.js
+++ next.config.js
@@ -6,10 +6,20 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
+const S3_ENDPOINT = process.env.S3_ENDPOINT
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
+      ...[S3_ENDPOINT].filter(Boolean).map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
 
